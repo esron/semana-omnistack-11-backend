c84272812b77bab0bad013e3c5d8fad861293a74
Esron Dtamar <esron.dtamar@gmail.com>
2020-03-26 00:45:47 -0300

    lógica de loguot

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiPower, FiTrash2 } from 'react-icons/fi';
 import './styles.css';
 
@@ -9,6 +9,8 @@ import logoImg from '../../assets/logo.svg';
 
 export default function Profile() {
   const [incidents, setIncidents] = useState([]);
+
+  const history = useHistory();
 
   const ongId = localStorage.getItem('ongId');
   const ongName = localStorage.getItem('ongName');
@@ -37,6 +39,12 @@ export default function Profile() {
     }
   }
 
+  function handleLogout() {
+    localStorage.clear();
+
+    history.push('/');
+  }
+
   return (
     <div className="profile-container">
       <header>
@@ -44,7 +52,7 @@ export default function Profile() {
         <span>Bem vinda, {ongName}</span>
 
         <Link className="button" to="/incidents/new">Cadastrar novo Caso</Link>
-        <button type="button">
+        <button type="button" onClick={handleLogout}>
           <FiPower size={18} color="#e02041" />
         </button>
       </header>
