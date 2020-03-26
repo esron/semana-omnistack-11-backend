dcb8f01f8b24725fba71e5102946d4a7f46ce736
Esron Dtamar <esron.dtamar@gmail.com>
2020-03-26 00:24:59 -0300

    usa o nome da ong logada

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -6,11 +6,12 @@ import './styles.css';
 import logoImg from '../../assets/logo.svg';
 
 export default function Profile() {
+  const ongName = localStorage.getItem('ongName');
   return (
     <div className="profile-container">
       <header>
         <img src={logoImg} alt="Be the Hero"/>
-        <span>Bem vinda, APAD</span>
+        <span>Bem vinda, {ongName}</span>
 
         <Link className="button" to="/incidents/new">Cadastrar novo Caso</Link>
         <button type="button">
