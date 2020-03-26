ca5841c9dcd5096b9da221b62a068c63aae13555
Esron Dtamar <esron.dtamar@gmail.com>
2020-03-26 00:05:30 -0300

    configura cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const cors = require('cors')
 const routes = require('./routes')
 
 const app = express()
 const port = 3333
+
+app.use(cors())
 
 app.use(express.json())
 
