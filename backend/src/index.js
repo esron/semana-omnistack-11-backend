ab791e93bd5102d343c2e1cb488acfe1dc36098b
Esron Dtamar <esron.dtamar@gmail.com>
2020-03-27 18:38:05 -0300

    configura errors do celebrate

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const cors = require('cors')
+const { errors } = require('celebrate')
 const routes = require('./routes')
 
 const app = express()
 const port = 3333
 
 app.use(cors())
-
 app.use(express.json())
-
 app.use(routes)
+app.use(errors())
 
 app.listen(port,  () => console.log(`Be The HERO is listening on port ${port}!`))
