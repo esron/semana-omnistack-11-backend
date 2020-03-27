522f154c89098cd251072d46f4bbe3ca2c245227
Esron Dtamar <esron.dtamar@gmail.com>
2020-03-27 19:43:38 -0300

    fix validações de ong

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -13,8 +13,8 @@ routes.get('/ongs', OngController.index)
 routes.post('/ongs', celebrate({
   [Segments.BODY]: Joi.object().keys({
     name: Joi.string().required(),
-    email: Joi.string().required().email,
-    whatsapp: Joi.number().required().min(10).max(11),
+    email: Joi.string().required().email(),
+    whatsapp: Joi.string().required().min(10).max(11),
     city: Joi.string().required(),
     uf: Joi.string().required().length(2),
   })
