325c236180dcc01290f7135666d0ddc32f45389e
Esron Dtamar <esron.dtamar@gmail.com>
2020-03-26 23:48:35 -0300

    fix typo

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -37,7 +37,7 @@ export default function Incidents() {
         </Text>
       </View>
 
-      <Text style={styles.title}>Bem-vindo1</Text>
+      <Text style={styles.title}>Bem-vindo!</Text>
       <Text style={styles.description}>Escolha um dos casos abaixo e salve o dia.</Text>
 
       <FlatList
